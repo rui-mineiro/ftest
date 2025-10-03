36a08e457ba98b3ff2e10c8cd4ec0345363ae5bc
Rui Mineiro <rui.mineiro@gmail.com>
2025-10-03 15:52:26 +0100

    erwerwer

--- test00.py
+++ test00.py
@@ -1,17 +1,21 @@
+
 import pandas as pd
+import numpy as np
 
-data = {
-    "Low": [21.233999, 20.112000, 19.965000, 20.906000, 20.997999]
-}
+tickerIdx = ['DAVV.DE', 'VETH.DE']
+TradeAction = ['Buy', 'Sell']
+date_index = pd.date_range('2024-10-01', periods=5, freq='B')
 
-dates = pd.to_datetime([
-    "2025-09-24",
-    "2025-09-25",
-    "2025-09-26",
-    "2025-09-29",
-    "2025-09-30"
-])
 
-df = pd.DataFrame(data, index=dates)
-df.index.name = "Date"
+cols = pd.MultiIndex.from_product([TradeAction, tickerIdx],names=['Trade', 'Ticker'])
+moved = pd.DataFrame(np.nan,index=date_index,columns=cols)
+
+print(moved)
+
+
+# tickerIdx
+# ['DAVV.DE', 'VETH.DE']
+# 
+# I need to create multiindex dataframe called moved with level 0 called BuySell and level 1 tickerIdx
+
 
